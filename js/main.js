6aa84c5a692032148e322c5c1e8dd50b5b5652ae
Yuko Okamoto <y.okamoto.0227@gmail.com>
2023-03-22 01:48:00 +0900

    一旦保存

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+/*ハンバーガーメニュー*/
 $(".header_nav-switch-items").click(function () {
     $(this).toggleClass('active');
+});
+
+/*mainvisual*/
+$('.slider').slick({
+    fade:true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed:1000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+    $('.slider').slick('slickPlay');
 });
