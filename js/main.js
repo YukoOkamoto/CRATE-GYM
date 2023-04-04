e00589a1b66258e3ab8b4ca081a5db29d8aade97
Yuko Okamoto <y.okamoto.0227@gmail.com>
2023-04-04 15:23:13 +0900

    jsの修正・ボタンアニメーション追加・リンク先ページの位置修正・タイトル をspanに修正

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ $(function(){
     $(".header_nav_btn").click(function(){
         $(this).toggleClass('active');
         $(".slide_in").toggleClass('open');
-        $(".slide_in").fadetoggle(1000);
     });
 
-    $(".header_nav_btn,.nav-menu li a").click(function(){
-        $(".slide_in").hide();
+    $(".nav-menu li a").click(function(){
+        $(".slide_in").removeClass('open');
+        $(".header_nav_btn").removeClass('active');
     });
 
     $(".slider").slick({
